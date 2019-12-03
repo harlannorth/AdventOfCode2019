fe6223abe398a86fe783bbd16b232a87db006a43
h <harlannorth@gmail.com>
2019-12-03 15:07:15 -0800

    Challenge 1 Completed

---
+++ Day2/Challenge1/Code/Challenge1/Intcode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Challenge1
+{
+    public class Intcode
+    {
+        private const char delimiter = ',';
+
+        public int[] RunIntCode(string input)
+        {
+            var program = ParseInput(input);
+
+            var currentPosition = 0;
+            var complete = false;
+            do
+            {
+                var opCode = program[currentPosition];
+                switch(opCode)
+                {
+                    case 1:
+                        program = PerformOperationOne(currentPosition, program);
+                        break;
+                    case 2:
+                        program = PerformOperationTwo(currentPosition, program);
+                        break;
+                    case 99:
+                    default:
+                        complete = true;
+                        break;
+                }
+                currentPosition = currentPosition + 4;
+
+            }
+            while( !complete );
+            
+            return program;
+        }
+
+        public int[] ParseInput(string input)
+        {
+            var split = input.Split(delimiter);
+            var program = new List<int>();
+
+            foreach(var s in split)
+            {
+                if (int.TryParse(s.Trim(), out var code))
+                {
+                    program.Add(code);
+                }
+            }
+
+            return program.ToArray();
+
+        }
+
+        public int[] PerformOperationOne(int position, int[] program)
+        {
+            var newValue = program[program[position+1]] + program[program[position+2]];
+            
+            program[program[position+3]] = newValue;
+
+            return program;
+        }
+        
+        public int[] PerformOperationTwo(int position, int[] program)
+        {
+            var newValue = program[program[position+1]] * program[program[position+2]];
+            
+            program[program[position+3]] = newValue;
+            
+            return program;
+        }
+
+    }
+}
---
+++ Day2/Challenge1/Test/Challenge1Test/IntercodeTests.cs
@@ -0,0 +1,85 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenge1;
+using System;
+using System.Diagnostics;
+
+namespace Challenge1Test
+{
+    [TestClass]
+    public class IntercodeTests
+    {
+
+        private Intcode _intcode;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _intcode = new Intcode();
+        }
+
+        [TestMethod]
+        public void TestRunWith99()
+        {
+            var result = _intcode.RunIntCode("99");
+            //Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            Trace.WriteLine("Hello World");
+            Assert.AreEqual(99,result[0]);
+        }
+
+        //1,0,0,0,99
+        [TestMethod]
+        public void TestRunWithOneResult()
+        {
+            var result = _intcode.RunIntCode("1,0,0,0,99");
+            Assert.AreEqual(2,result[0]);
+        }
+
+        //1,1,1,4,99,5,6,0,99
+        [TestMethod]
+        public void TestRunWithTwoResults()
+        {
+            var result = _intcode.RunIntCode("1,1,1,4,99,5,6,0,99");
+            Assert.AreEqual(30,result[0]);
+            Assert.AreEqual(2,result[4]);
+        }
+
+        //2,3,0,3,99
+        [TestMethod]
+        public void TestRunWithOneMidResult()
+        {
+            var result = _intcode.RunIntCode("2,3,0,3,99");
+            Assert.AreEqual(6,result[3]);
+        }
+
+        [TestMethod]
+        public void TestRunWithOneEndResult()
+        {
+            var result = _intcode.RunIntCode("2,4,4,5,99,0");
+            Assert.AreEqual(9801,result[5]);
+        }
+
+        [TestMethod]
+        public void TestParse()
+        {
+            var program = _intcode.ParseInput("1,2,3,");
+            Assert.IsNotNull(program);
+            Assert.AreEqual(1,program[0]);
+            Assert.AreEqual(3,program[2]);
+        }
+
+        [TestMethod]
+        public void PerformOperationOneTest()
+        {
+            var result = _intcode.PerformOperationOne(0, new int[] {1,9,10,3,2,3,11,0,99,30,40,50});
+            Assert.AreEqual(70,result[3]);
+        }
+
+        [TestMethod]
+        public void PerformOperationTwoTest()
+        {
+            var result = _intcode.PerformOperationTwo(0, new int[] {2,9,10,3,2,3,11,0,99,30,40,50});
+            Assert.AreEqual(1200,result[3]);
+        }
+    }
+}
