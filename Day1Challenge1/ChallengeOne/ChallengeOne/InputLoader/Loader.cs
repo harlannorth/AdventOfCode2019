40b0d68e4d12d319817f8c1856520afb4e441d5d
h <harlannorth@gmail.com>
2019-12-02 16:47:11 -0800

    Initial check in

---
+++ Day1Challenge1/ChallengeOne/ChallengeOne/InputLoader/Loader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ChallengeOne.InputLoader
+{
+    internal class Loader : IInputLoader
+    {
+        public IEnumerable<int> LoadFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileLoadException("File doesn't exist");
+            }
+
+            var reader = File.ReadAllLines(fileName);
+
+            var inputs = new List<int>();
+            foreach(var line in reader)
+            {
+                if (int.TryParse(line, out var value))
+                {
+                    inputs.Add(value);
+                }
+            }
+            return inputs;
+        }
+    }
+}
---
+++ Day1Challenge1/ChallengeOne/ChallengeOne/Program.cs
@@ -0,0 +1,37 @@
+﻿using ChallengeOne.InputLoader;
+using System;
+
+namespace ChallengeOne
+{
+    class Program
+    {
+        private const int divisor = 3;
+        private const int subtract = 2;
+
+        static void Main(string[] args)
+        {
+            
+            if (args == null || args.Length == 0)
+            {
+                throw new Exception("No file to load.");
+            }
+
+            Console.WriteLine(args[0]);
+
+            IInputLoader loader = new Loader();
+            var inputs = loader.LoadFile(args[0]);
+
+            if (inputs == null)
+                throw new Exception("Parsed file returned no input");
+
+            int result = 0;
+            foreach(var mass in inputs)
+            {
+                var dividedMass = (int)(mass / divisor) - subtract;
+                result += dividedMass;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
